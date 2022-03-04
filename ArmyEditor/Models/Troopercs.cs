e3f3ea9ab84b191335af7de8a9a40578d7001be0
hallgato <hallgato@LABOR210-01>
2022-03-04 13:26:42 +0100

    Add project files.

---
+++ ArmyEditor/Models/Troopercs.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyEditor.Models
+{
+   public class Troopercs : ObservableObject
+    {
+        private string _type;
+        public string Type 
+        { get => _type;
+          set => SetProperty(ref _type, value);
+        }
+        private int _power;
+        public int Power 
+        {
+            get => _power;
+            set => SetProperty(ref _power, value); 
+        }
+        private int _speed;
+        public int Speed 
+        {
+            get => _speed;
+            set => SetProperty(ref _speed, value);
+        }
+    }
+}
---
+++ ArmyEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,92 @@
+﻿using ArmyEditor.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyEditor.ViewModels
+{
+   public class MainWindowViewModel : ObservableRecipient
+    {
+        public ObservableCollection<Troopercs> Barracks { get; set; }
+        public ObservableCollection<Troopercs> Army { get; set; }
+
+        private Troopercs _selectedItemFromBarracks;
+
+        public Troopercs SelectedItemFromBarracks 
+        {
+            get => _selectedItemFromBarracks;
+            set 
+            {
+                SetProperty(ref _selectedItemFromBarracks, value);
+                AddArmyCommand.NotifyCanExecuteChanged();
+            } 
+        }
+        private Troopercs _selectedItemFromArmy;
+
+        public Troopercs SelectedItemFromArmy 
+        {
+            get => _selectedItemFromArmy;
+            set 
+            {
+                SetProperty(ref _selectedItemFromArmy, value);
+
+                RemoveFromArmyCommand.NotifyCanExecuteChanged();
+            } 
+        }
+
+        public RelayCommand AddArmyCommand { get; set; }
+        public RelayCommand RemoveFromArmyCommand { get; set; }
+
+        public MainWindowViewModel() 
+        {
+            Barracks = new ObservableCollection<Troopercs>
+            {
+                new Troopercs
+                {
+                    Type = "marine",
+                    Power = 5,
+                    Speed = 5
+                },
+                new Troopercs
+                {
+                    Type = "sniper",
+                    Power = 8,
+                    Speed = 3
+                },
+                new Troopercs
+                {
+                    Type = "Pilot",
+                    Power = 5,
+                    Speed = 8
+                },
+                new Troopercs
+                {
+                    Type = "Engineer",
+                    Power = 1,
+                    Speed = 7
+                },
+                new Troopercs
+                {
+                    Type = "Infentry",
+                    Power = 5,
+                    Speed = 7
+                },
+
+            };
+            Army = new ObservableCollection<Troopercs>();
+
+            AddArmyCommand = new RelayCommand(() => 
+            {
+                Army.Add(SelectedItemFromBarracks);
+                SelectedItemFromBarracks = null;
+            }
+            ,()=> SelectedItemFromBarracks !=null);
+            RemoveFromArmyCommand = new RelayCommand(() => Army.Remove(SelectedItemFromArmy),()=> SelectedItemFromArmy != null);
+        }
+    }
+}
